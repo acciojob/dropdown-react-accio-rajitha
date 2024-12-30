24521d9a95c3f34959b354365c8dfaff9c5aefae
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-30 16:40:13 +0530

    code2

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,8 @@ const states = [
 		},
 		{
 		  name: "Bhopal",
-		  description: "Bhopal is a city in the central Indian state of Madhya Pradesh. It is one of India’s greenest cities. There are two main lakes, the Upper Lake and the Lower Lake.",
+		  description: "DBhopal is a city in the central Indian state of Madhya Pradesh. It's one of India's greenest city. There are two main lakes, the Upper Lake and the Lower Lake.", 
+		  
 		  
 		  landmarks: [
 			{
